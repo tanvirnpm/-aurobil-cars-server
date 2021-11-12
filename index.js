5adf55b89f965ef3c003202ee9a2fa952d01a2ce
tanvirnpm <tanvirnpm@gmail.com>
2021-11-13 04:19:16 +0600

    port change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ObjectId = require('mongodb').ObjectId;
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const port = 5000
+const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(bodyParser.json())
 
